0344a2777fb81f04820a5bc42e42e99a3187cd3e
Cloud Le <lehaoson@gmail.com>
2021-03-30 03:58:56 +0700

    re-run

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "app" {
-  bucket = "terraform-cloud-playground"
+  bucket = "terraform-cloud-playground-1"
 
   website {
     index_document = "index.html"
